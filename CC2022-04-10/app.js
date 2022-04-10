561e35d3e6a213c9da026329d73ff09876641c24
techyounglogy <techyounglogy@gmail.com>
2022-04-10 12:05:52 -0400

    Description of Challenges

---
+++ CC2022-04-10/app.js
@@ -0,0 +1,15 @@
+// Convert a Number to a String!
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+function numberToString(num) {
+    return String(num)
+}
+
+// Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord(bool) {
+    
+}
