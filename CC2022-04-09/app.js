3574b1ce32438d00c1cae1025971bd0cc55b204d
techyounglogy <techyounglogy@gmail.com>
2022-04-09 23:44:57 -0400

    Solution of first challenge and Description of secon challenge

--- CC2022-04-09/app.js
+++ CC2022-04-09/app.js
@@ -14,4 +14,22 @@
 // in other words, it is the product of some integer with itself.
 
 // The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+var isSquare = function (n) {
+    return Math.sqrt(n) % 1 === 0
+}
 
+// Array.diff
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+function arrayDiff(a, b) {
+    
+}
