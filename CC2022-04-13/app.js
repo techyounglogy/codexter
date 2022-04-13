569087442d5226dd891b592d1bdfe006f0da698c
techyounglogy <techyounglogy@gmail.com>
2022-04-13 18:07:39 -0400

    Cleaver solution for 2022-04-13

--- CC2022-04-13/app.js
+++ CC2022-04-13/app.js
@@ -57,3 +57,9 @@ function findOutlier(integers) {
         return oddA[0]
     }
 }
+// Cleaver Solution
+// function findOutlier(int) {
+//     var even = int.filter(a => a % 2 == 0);
+//     var odd = int.filter(a => a % 2 !== 0);
+//     return even.length == 1 ? even[0] : odd[0];
+// }
