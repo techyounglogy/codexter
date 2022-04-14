70d7366d6a5612055c5d8748cd81102806c15189
techyounglogy <techyounglogy@gmail.com>
2022-04-14 18:58:01 -0400

    oding challenges and solutions for 2022-04-14

--- CC2022-04-14/app.js
+++ CC2022-04-14/app.js
@@ -10,3 +10,22 @@
 // "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
 // "aA11" -> 2 # 'a' and '1'
 // "ABBA" -> 2 # 'A' and 'B' each occur twice
+function duplicateCount(text) {
+    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+
+// 若text=“Abccdeda”
+
+// .toLowerCase()                               将text全变成小写  “abccdeda”
+
+// .split('')  以‘’规则分割                     [‘a’, 'b','c','c','d','e','d','a']
+
+// .sort() 排序                                 ['a','a','b','c','c','d','d','e']
+
+// .join() 将数组中所有元素放在一起 'aabccdde'
+
+// .match(/([^])\1+/g)                          将符合正则的放在新数组中 ['aa','cc','dd']  
+
+// /([^])\1+/g                                  g表示全局，/... /表示正则内容，[^]排除，\1+至少出现一次，，，返回全局中重复出现的
+
+// .length                                      求数组的长度
