ba6a9d63e92f8e27af51459228567b4b495dd18d
techyounglogy <techyounglogy@gmail.com>
2022-04-18 21:08:02 -0400

    solutions for 2022-04-18

--- CC2022-04-18/lc.js
+++ CC2022-04-18/lc.js
@@ -20,3 +20,21 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
+var twoSum = function (nums, target) {
+    // Array to store the result
+    result = [];
+    // Map to store the difference and its index
+    index_map = new Map();
+    // Loop for each element in the array
+    for (let i = 0; i < nums.length; i++) {
+        let difference = target - nums[i];
+        if (index_map.has(difference)) {
+            result[0] = i;
+            result[1] = index_map.get(difference);
+            break;
+        } else {
+            index_map.set(nums[i], i);
+        }
+    }
+    return result;
+}
