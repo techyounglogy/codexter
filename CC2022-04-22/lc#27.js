a2b35303e7f239aa1842d424e1377a50af60d102
techyounglogy <techyounglogy@gmail.com>
2022-04-22 20:58:35 -0400

    solution for #27

--- CC2022-04-22/lc#27.js
+++ CC2022-04-22/lc#27.js
@@ -26,7 +26,7 @@
 // }
 // If all assertions pass, then your solution will be accepted.
 
- 
+
 
 // Example 1:
 
@@ -42,3 +42,14 @@
 // Note that the five elements can be returned in any order.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
+var removeElement = function (nums, val) {
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[j++] = nums[i];
+        }
+    }
+
+    //nums.length = j; // Chop the surplus values - not needed for LC submission
+    return j;
+};
