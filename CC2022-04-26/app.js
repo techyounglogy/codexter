db2e338168b2d51737bd8a773b64bdefc8441862
techyounglogy <techyounglogy@gmail.com>
2022-04-25 18:51:51 -0400

    solution for codewar challenge

--- CC2022-04-26/app.js
+++ CC2022-04-26/app.js
@@ -12,3 +12,23 @@
 // (-1, 0) --> -1 (-1 + 0 = -1)
 // (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 
+function getSum(a, b) {
+    let total = 0;
+    if (a === b) {
+        return a;
+    }
+
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            total += i;
+        }
+        return total;
+    }
+
+    if (b < a) {
+        for (let i = b; i <= a; i++) {
+            total += i;
+        }
+        return total;
+    }
+}
