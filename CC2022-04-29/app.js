50af1a273af47f853c4dda763d11878d0616cc12
techyounglogy <techyounglogy@gmail.com>
2022-04-28 20:42:21 -0400

    solution for codewars challenge

--- CC2022-04-29/app.js
+++ CC2022-04-29/app.js
@@ -12,3 +12,14 @@
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
+function alphabetPosition(text) {
+    const upperText = text.toUpperCase()
+    const arr = upperText.split("").map(let => let.charCodeAt())
+    const newArr = arr.filter(num => {
+        if (num > 64 && num < 91) {
+            return num
+        }
+    })
+    const updateNumArr = newArr.map(num => num - 64)
+    return updateNumArr.join(" ")
+}
