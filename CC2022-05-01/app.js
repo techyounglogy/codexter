35cbeae1cadb0c46c6249df4294818e54eae6455
techyounglogy <techyounglogy@gmail.com>
2022-04-30 21:00:06 -0400

    another description for codewars challenge

--- CC2022-05-01/app.js
+++ CC2022-05-01/app.js
@@ -28,3 +28,18 @@ function maskify(cc) {
     }
     return ccArr.join('')
 }
+
+// Grasshopper - Summation
+
+// Summation
+// Write a program that finds the summation of every number from 1 to num. 
+// The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
