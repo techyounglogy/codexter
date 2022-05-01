fb55edfcde42a6652b0a79c5c3d863eb6b2ec44a
techyounglogy <techyounglogy@gmail.com>
2022-04-30 21:00:43 -0400

    solution for codewars challenge

--- CC2022-05-01/app.js
+++ CC2022-05-01/app.js
@@ -43,3 +43,10 @@ function maskify(cc) {
 // summation(8) -> 36
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
+var summation = function (num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
