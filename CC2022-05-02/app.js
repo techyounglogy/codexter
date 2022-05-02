5ce4bff9181eeb3b004141ba08f2779580c47116
techyounglogy <techyounglogy@gmail.com>
2022-05-01 21:13:10 -0400

    solution explained for codewars challenge

--- CC2022-05-02/app.js
+++ CC2022-05-02/app.js
@@ -24,3 +24,23 @@ function squareSum(numbers) {
 // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+function uniqueInOrder(iterable) {
+    // The first thing we do is create a variable called strArr. 
+    // In this variable, we could go ahead and use this to convert our string to an array. 
+    // The problem is not all of the function’s inputs will be strings. 
+    // We will need to check if the input is an array or not. 
+    // If it is an array, we directly assign the input to strArr.
+
+    // If it is not and it is a string, 
+    // we convert the string to an array and then assign the array to strArr.
+    let strArr = Array.isArray(iterable) ? iterable : iterable.split('');
+    // Next, we create another variable called unique. 
+    // This variable will contain our unique array without the consecutive duplicate array elements. 
+    // We use the filter() method to filter out any elements with the same value next to each other 
+    // and keep only one of them.
+    let unique = strArr.filter((letter, i) => {
+        return strArr[i] != strArr[i + 1];
+    })
+    return unique;
+}
