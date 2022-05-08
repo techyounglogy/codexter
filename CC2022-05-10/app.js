2f7880829fc2c54b091b4d3628041368ca46f506
techyounglogy <techyounglogy@gmail.com>
2022-05-08 19:52:57 -0400

    solution for codewars challenge

--- CC2022-05-10/app.js
+++ CC2022-05-10/app.js
@@ -83,3 +83,12 @@ function litres(time) {
 
 // s="aaaxbbbbyyhwawiwjjjwwm"
 // printer_error(s) => "8/22"
+
+function printerError(s) {
+    const dic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'];
+    const splitString = s.split('');
+    let error = 0;
+    splitString.map((x) => dic.includes(x) === false ? error += 1 : error += 0);
+
+    return `${error}/${s.length}`;
+}
