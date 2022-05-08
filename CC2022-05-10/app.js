462ac8002a8d8bb902e51880eabe822a053f64d9
techyounglogy <techyounglogy@gmail.com>
2022-05-08 19:37:07 -0400

    description for codewars challenge

--- CC2022-05-10/app.js
+++ CC2022-05-10/app.js
@@ -12,3 +12,19 @@
 function validatePIN(pin) {
     return /^(\d{4}|\d{6})$/.test(pin);
 }
+
+// Sum of odd numbers
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
