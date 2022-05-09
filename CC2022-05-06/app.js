b130596123defa120616e7311cf8e96424133d49
techyounglogy <techyounglogy@gmail.com>
2022-05-09 19:38:00 -0400

    solution for codewars challenge

--- CC2022-05-06/app.js
+++ CC2022-05-06/app.js
@@ -1,6 +1,6 @@
 // Two to One
 
-// Take 2 strings s1 and s2 including only letters from ato z. 
+// Take 2 strings s1 and s2 including only letters from a to z. 
 // Return a new sorted string, the longest possible, 
 // containing distinct letters - each taken only once - coming from s1 or s2.
 
@@ -11,3 +11,7 @@
 
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    return [...new Set((s1 + s2).split(''))].sort().join('')
+}
