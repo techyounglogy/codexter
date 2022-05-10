7366ddb1657434173c64fc9a2b183410efe4e60d
techyounglogy <techyounglogy@gmail.com>
2022-05-10 19:21:53 -0400

    solution for codewars challenge

--- CC2022-05-09/app.js
+++ CC2022-05-09/app.js
@@ -8,3 +8,7 @@
 
 // 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+function addBinary(a, b) {
+    return (a + b).toString(2)
+}
