ee3191cea1360b79eb91e5b5096fba387448dc8a
techyounglogy <techyounglogy@gmail.com>
2022-05-10 19:55:59 -0400

    solution for codewars

--- CC2022-05-10/app.js
+++ CC2022-05-10/app.js
@@ -113,3 +113,9 @@ function printerError(s) {
 // input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
+function openOrSenior(data) {
+    function determineMembership(member) {
+        return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+    }
+    return data.map(determineMembership);
+}
