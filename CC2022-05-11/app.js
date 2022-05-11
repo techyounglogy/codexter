643dc3f61b67287d27596a8517e92641b1b68b33
techyounglogy <techyounglogy@gmail.com>
2022-05-11 14:37:56 -0400

    solution for codewar challenge

--- CC2022-05-11/app.js
+++ CC2022-05-11/app.js
@@ -2,3 +2,15 @@
 
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
+function moveZeros(arr) {
+    // TODO: Program me
+    const zeros = arr.filter((elm) => elm === 0).length;
+    const result = arr.filter((elm) => elm !== 0);
+
+    for (let i = 0; i < zeros; i += 1) {
+        result.push(0);
+    }
+
+    return result;
+}
+
